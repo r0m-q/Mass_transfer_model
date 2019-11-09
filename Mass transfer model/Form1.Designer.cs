760e24966004af40046f31d7e879cd8780106d69
Роман Науменко <romadelphi1996@gmail.com>
2019-11-09 19:09:24 +0200

    добавил конструкцию исключений

---
+++ Mass transfer model/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace Mass_transfer_model
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Output = new System.Windows.Forms.Button();
+            this.TextBoxTau = new System.Windows.Forms.TextBox();
+            this.TextBoxKol = new System.Windows.Forms.TextBox();
+            this.TextBoxTempl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.textBoxQv = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(30, 431);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(594, 64);
+            this.Output.TabIndex = 0;
+            this.Output.Text = "Посчитать";
+            this.Output.UseVisualStyleBackColor = true;
+            this.Output.Click += new System.EventHandler(this.Output_Click);
+            // 
+            // TextBoxTau
+            // 
+            this.TextBoxTau.BackColor = System.Drawing.SystemColors.Window;
+            this.TextBoxTau.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextBoxTau.Location = new System.Drawing.Point(102, 129);
+            this.TextBoxTau.Name = "TextBoxTau";
+            this.TextBoxTau.Size = new System.Drawing.Size(145, 20);
+            this.TextBoxTau.TabIndex = 1;
+            this.TextBoxTau.Text = "0,2";
+            this.TextBoxTau.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TextBoxKol
+            // 
+            this.TextBoxKol.Location = new System.Drawing.Point(102, 184);
+            this.TextBoxKol.Name = "TextBoxKol";
+            this.TextBoxKol.Size = new System.Drawing.Size(145, 20);
+            this.TextBoxKol.TabIndex = 2;
+            this.TextBoxKol.Text = "6";
+            this.TextBoxKol.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TextBoxTempl
+            // 
+            this.TextBoxTempl.Location = new System.Drawing.Point(102, 241);
+            this.TextBoxTempl.Name = "TextBoxTempl";
+            this.TextBoxTempl.Size = new System.Drawing.Size(145, 20);
+            this.TextBoxTempl.TabIndex = 3;
+            this.TextBoxTempl.Text = "0";
+            this.TextBoxTempl.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Время";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Количество узлов";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Начальная температура";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(389, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(158, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Выберите граничные условия";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(68, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(223, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Установите необходимые Вам параметры";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Mass_transfer_model.Properties.Resources.формула7;
+            this.pictureBox1.Location = new System.Drawing.Point(392, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(208, 35);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Mass_transfer_model.Properties.Resources.формула5;
+            this.pictureBox3.Location = new System.Drawing.Point(392, 124);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(208, 35);
+            this.pictureBox3.TabIndex = 17;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Mass_transfer_model.Properties.Resources.формула4;
+            this.pictureBox4.Location = new System.Drawing.Point(392, 299);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(222, 62);
+            this.pictureBox4.TabIndex = 18;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Mass_transfer_model.Properties.Resources.формула2;
+            this.pictureBox5.Location = new System.Drawing.Point(392, 233);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(218, 55);
+            this.pictureBox5.TabIndex = 19;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Mass_transfer_model.Properties.Resources.формула1;
+            this.pictureBox2.Location = new System.Drawing.Point(392, 168);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(218, 54);
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(371, 92);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(14, 13);
+            this.radioButton1.TabIndex = 21;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(371, 138);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(14, 13);
+            this.radioButton2.TabIndex = 22;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(371, 191);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(14, 13);
+            this.radioButton3.TabIndex = 23;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(371, 255);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(14, 13);
+            this.radioButton4.TabIndex = 24;
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(371, 325);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(14, 13);
+            this.radioButton5.TabIndex = 25;
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // textBoxQv
+            // 
+            this.textBoxQv.Location = new System.Drawing.Point(102, 297);
+            this.textBoxQv.Name = "textBoxQv";
+            this.textBoxQv.Size = new System.Drawing.Size(145, 20);
+            this.textBoxQv.TabIndex = 26;
+            this.textBoxQv.Text = "0";
+            this.textBoxQv.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(113, 281);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Внутренний источник Qv";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 507);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxQv);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBoxTempl);
+            this.Controls.Add(this.TextBoxKol);
+            this.Controls.Add(this.TextBoxTau);
+            this.Controls.Add(this.Output);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Output;
+        private System.Windows.Forms.TextBox TextBoxTau;
+        private System.Windows.Forms.TextBox TextBoxKol;
+        private System.Windows.Forms.TextBox TextBoxTempl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.TextBox textBoxQv;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Mass transfer model/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace Mass_transfer_model
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public double[,] Temp = new double[1000, 1000];
+        public double[] a = new double[1000];
+        public double[] b = new double[1000];
+        public double[] c = new double[1000];
+        public double[] d = new double[1000];
+        public double[] f = new double[1000];
+        public double[] g = new double[1000];
+
+        private void Output_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application ObjExcel = new Microsoft.Office.Interop.Excel.Application();
+            Workbook ObjWorkBook;
+            Worksheet ObjWorkSheet;
+            //Книга.
+            ObjWorkBook = ObjExcel.Workbooks.Add(System.Reflection.Missing.Value);
+            //Таблица.
+            ObjWorkSheet = (Worksheet)ObjWorkBook.Sheets[1];
+            //ObjWorkSheet = ObjWorkBook.ActiveSheet as Microsoft.Office.Interop.Excel.Worksheet;
+            double Tau = Convert.ToDouble(TextBoxTau.Text);//время
+            int Kol = Convert.ToInt32(TextBoxKol.Text);//кол-во узлов
+            double Templ = Convert.ToDouble(TextBoxTempl.Text);//начальная температура
+            double qv = Convert.ToDouble(textBoxQv.Text);//Внутренний источник
+
+            double dt = 0.04;//шаг по времени
+            double lam = 1;
+            double C = 1;
+            double ro = 1;
+            double tau = 0;
+            double l = 1.0;//принимаем длинну за 1
+            double h = l / (Kol-1);//считаем шаг сетки
+
+
+            Kol = Kol - 1;
+            for (int k = 0; k <= Kol; k++)
+            {
+                Temp[0, k] = Templ;
+                ObjWorkSheet.Cells[3, k + 3] = Temp[0, k];//выводим начальную температуру во всех узлах
+            }
+            ObjWorkSheet.Cells[3, 1] = "Time" + (0);//оформляем вывод
+            ObjWorkSheet.Cells[3, 2] = tau;
+            int i = 1;
+            double x = 0;
+            int j = 0;
+            tau = tau + dt;
+            while (tau <= Tau)
+            {
+                for (int k = 1; k <= Kol - 1; k++)//считаем коэфициенты для внутренних узлов
+                {
+                    a[k] = (dt * lam) / (C * ro * h * h);
+                    c[k] = (dt * lam) / (C * ro * h * h);
+                    b[k] = -1 - 2 * (dt * lam) / (C * ro * h * h);
+                    d[k] = Temp[i - 1, k] + dt / (C * ro) * qv;
+                }
+                //граничые условия
+
+                if (radioButton1.Checked)
+                {
+                    //первого рода слева=0 справа=0
+                    a[0] = 0;
+                    b[0] = 1;
+                    d[0] = 0;
+                    b[Kol] = 1;
+                    c[Kol] = 0;
+                    d[Kol] = 0;
+                }
+                else if (radioButton2.Checked)
+                {
+                    //первого рода слева=0 справа=1
+                    a[0] = 0;
+                    b[0] = 1;
+                    d[0] = 0;
+                    b[Kol] = 1;
+                    c[Kol] = 0;
+                    d[Kol] = -1;
+                }
+                //if (radioButton.Checked)
+                //{
+                //    //первого рода слева=1 справа=1
+                //    a[0] = 0;
+                //    b[0] = 1;
+                //    d[0] = -1;
+                //    b[Kol] = 1;
+                //    c[Kol] = 0;
+                //    d[Kol] = -1;
+                //}
+                else if (radioButton3.Checked)
+                {
+                    //первого рода слева=0 второго рода справа=0
+                    a[0] = 0;
+                    b[0] = 1;
+                    d[0] = 0;
+                    b[Kol] = 1 / h;
+                    c[Kol] = -1 / h;
+                    d[Kol] = 0;
+                }
+                else if (radioButton4.Checked)
+                {
+                    //первого рода слева=0 второго рода справа=1 
+                    a[0] = 0;
+                    b[0] = 1;
+                    d[0] = 0;
+                    b[Kol] = 1 / h;
+                    c[Kol] = -1 / h;
+                    d[Kol] = -1;
+                }
+                else
+                {
+                    a[0] = 0;
+                    b[0] = 1;
+                    d[0] = -1;
+                    b[Kol] = 1 / h;
+                    c[Kol] = -1 / h;
+                    d[Kol] = -1;
+                }
+
+                f[0] = -a[0] / b[0];
+                g[0] = -d[0] / b[0];
+
+                for (int k = 1; k <= Kol; k++)
+                {
+                    f[k] = -a[k] / (b[k] + c[k] * f[k - 1]);
+                    g[k] = -(d[k] + c[k] * g[k - 1]) / (b[k] + c[k] * f[k - 1]);
+                }
+                Temp[i, Kol] = -(d[Kol] + c[Kol] * g[Kol - 1]) / (b[Kol] + c[Kol] * f[Kol - 1]);
+                for (int k = Kol - 1; k >= 0; k = k - 1)//подсчёт температуры в текущий момент времени i во всех узлах
+                {
+                    Temp[i, k] = Temp[i, k + 1] * f[k] + g[k];
+                }
+                for (int k = 0; k <= Kol; k++)
+                {
+                    x = j * h;
+                    ObjWorkSheet.Cells[1, j + 3] = "X" + (j + 1);
+                    ObjWorkSheet.Cells[2, j + 3] = x;
+                    ObjWorkSheet.Cells[i + 3, j + 3] = Temp[i, k];//вывод значений в таблицу exel в текуший момент времени і во всех узлах
+                    j++;
+
+                    //if (x > 1) { break; }
+                }
+                j = 0;
+                ObjWorkSheet.Cells[i + 3, 1] = "Time" + i;
+                ObjWorkSheet.Cells[i + 3, 2] = tau;
+                tau = tau + dt;
+                i++;
+                //Вызываем эксель.
+                ObjExcel.Visible = true;
+                ObjExcel.UserControl = true;
+            }
+        }
+    }
+    
+}
